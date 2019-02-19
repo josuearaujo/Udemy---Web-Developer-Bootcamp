560d1ac8a46da8585b68c4e71aececffc1af9336
Josue Araujo <032342791694>
2019-02-19 19:10:29 -0300

    More lessons

--- Seção 18 - jQuery/exercise.js
+++ Seção 18 - jQuery/exercise.js
@@ -8,4 +8,4 @@ $("div.highlight").css("width", "200px");
 $("#third").css("border", "4px solid orange");
 
 // Bonus: Select the first div only and change its font color to pin
-$("div:first-of-type").css("color", "pink");     
+$("div:first-of-type").css("color", "pink");       
---
+++ Seção 19 - Advanced jQuery/effects.js
@@ -0,0 +1,7 @@
+$("button").on("click", function(){
+	$("div").fadeToggle(1000, function(){
+		//$(this).remove();
+	});
+})
+ 
+
