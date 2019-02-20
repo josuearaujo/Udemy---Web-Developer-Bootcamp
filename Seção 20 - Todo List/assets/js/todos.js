5270c4100f9a8710a01ea91a5e15341f11ec5c0d
Josue Araujo <032342791694>
2019-02-20 14:38:47 -0300

    ToDo List

--- Seção 20 - Todo List/assets/js/todos.js
+++ Seção 20 - Todo List/assets/js/todos.js
@@ -1,1 +1,21 @@
-alert("CONNECTED!");
+$("ul").on("click", "li", function(){
+	//$(this).css("text-decoration", "line-through");
+	$(this).toggleClass("finished");
+});
+
+$("ul").on("click", "li span", function(event){
+	event.stopPropagation();
+	$(this).parent("li").fadeOut(function(){
+		$(this).remove();
+	})
+})
+
+$("input[type='text']").on("keypress", function(event){
+	if(event.which === 13) {
+		if($(this).val() !== ""){
+			$("ul").append("<li><span>X</span> " + $(this).val() + "</li>");
+			$(this).val("");
+		}
+	}
+})
+
